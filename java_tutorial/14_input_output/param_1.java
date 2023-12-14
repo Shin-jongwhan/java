71cd49ff1decfd1b5acaa0b57df94c5bd537150a
Shin-jonghwan <62974484+Shin-jongwhan@users.noreply.github.com>
2023-12-14 17:01:49 +0900

    .

---
+++ java_tutorial/14_input_output/param_1.java
@@ -0,0 +1,9 @@
+//package 14_input_output;
+
+public class param_1 {
+    public static void main(String[] args) {
+        for (String i : args) {
+            System.out.println(i);
+        }
+    }
+}
