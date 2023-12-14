9ff0e1bb32a25d068c97deea6228dc9ad2949500
Shin-jonghwan <62974484+Shin-jongwhan@users.noreply.github.com>
2023-12-14 10:29:01 +0900

    .

---
+++ java_tutorial/3_comment/comment.java
@@ -0,0 +1,18 @@
+public class comment {
+    public static void main(String[] args) {
+        // 이와 같이 한 줄만 주석 처리가 가능하다.
+        String a, b;
+        a = "a";
+        b = "b";
+
+        System.out.println(b);
+        System.out.println(a);
+        /*
+         * 여러 줄 주석 작성
+         * 이렇게 주석을 여러 줄 달 수 있다.
+         * 기본적으로 이렇게 문서화를 할 수 있다.
+         * 
+         * 이 스크립트에 대한 설명을 적고, 어떤 식으로 돌아가고 등을 적는다.
+         */
+    }
+}
