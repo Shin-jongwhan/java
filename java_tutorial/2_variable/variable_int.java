eb703d1b3690d39337499dbfd5d301f3cf667246
Shin-jonghwan <62974484+Shin-jongwhan@users.noreply.github.com>
2023-12-14 10:03:26 +0900

    variable

---
+++ java_tutorial/2_variable/variable_int.java
@@ -0,0 +1,10 @@
+public class variable_int {
+    public static void main(String[] args) {
+        int a;
+        a = 1;
+        System.out.println(a + 1); // 2
+        
+        a = 2;
+        System.out.println(a + 1); // 3
+    }
+}
