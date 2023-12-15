ab11b0ef92e6389268876d5519728b7ffa7141d4
Shin-jonghwan <62974484+Shin-jongwhan@users.noreply.github.com>
2023-12-15 17:01:17 +0900

    .

---
+++ java_tutorial/15_instance_and_object/example.java
@@ -0,0 +1,11 @@
+//package 15_instance_and_object;
+
+public class example {
+    public static void sum(int left, int right) {
+        System.out.println(left + right);
+    }
+    public static void main(String[] args) {
+        sum(10, 20);
+        sum(20, 40);
+    }
+}
