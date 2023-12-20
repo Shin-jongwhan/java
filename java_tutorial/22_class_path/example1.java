ab0f0144021acbe9ec04d2a995cb924b0b316989
Shin-jonghwan <62974484+Shin-jongwhan@users.noreply.github.com>
2023-12-20 13:28:52 +0900

    22

---
+++ java_tutorial/22_class_path/example1.java
@@ -0,0 +1,15 @@
+//package 22_class_path;
+
+class Item2{
+    public void print(){
+        System.out.println("Hello world");  
+    }
+}
+
+
+public class example1 {
+    public static void main(String[] args) {
+        Item2 i1 = new Item2();
+        i1.print();
+    }
+}
